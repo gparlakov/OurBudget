4dec51f0765f4288fe6a527f7791825ac8ac13e2
georgi.parlakov <gparlakov@gmail.com>
2015-07-06 18:15:12 +0300

    AddEditUserActivity started - not finished

--- app/src/main/java/com/parlakovi/petqjoro/ourbudget/activities/addEditUserActivity.java
+++ app/src/main/java/com/parlakovi/petqjoro/ourbudget/activities/addEditUserActivity.java
@@ -1,18 +1,51 @@
 package com.parlakovi.petqjoro.ourbudget.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.parlakovi.petqjoro.ourbudget.DBObjects.User;
+import com.parlakovi.petqjoro.ourbudget.MainActivity;
 import com.parlakovi.petqjoro.ourbudget.R;
 
 public class addEditUserActivity extends ActionBarActivity {
+
+    public static final String USER_ID_EXTRA = "user id extra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_user);
+
+        ActionBar bar = getSupportActionBar();
+        bar.setTitle(R.string.title_add_user);
+
+        final Activity self = this;
+
+        Button button = (Button)this.findViewById(R.id.button_addUser_OK);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User newUser = new User();
+
+                EditText editText_addUserName = (EditText)self.findViewById(R.id.editText_addUser_Name);
+
+                newUser.setName(editText_addUserName.getText().toString());
+
+                Intent resultIntent = getIntent();
+                resultIntent.putExtra(MainActivity.NEWLY_CREATED_USER, newUser);
+                self.setResult(RESULT_OK, resultIntent);
+
+                finish();
+            }
+        });
     }
 
     @Override
